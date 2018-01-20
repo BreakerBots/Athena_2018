c884bb005d273066c52e398c818be1474ce1e2fe
Ol' Dell <5104breakerbots@gmail.com>
2018-01-19 20:22:46 -0800

    Saved Encoder/Gyro/Joystick values to USB

--- subsystems/logging/Robot.java
+++ subsystems/logging/Robot.java
@@ -11,7 +11,14 @@ import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
+import com.kauailabs.navx.frc.AHRS;
+
+import edu.wpi.first.wpilibj.I2C;
 import edu.wpi.first.wpilibj.IterativeRobot;
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.SpeedControllerGroup;
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 
 /**
  * The VM is configured to automatically run this class, and to call the
@@ -26,36 +33,43 @@ public class Robot extends IterativeRobot {
 	 * This function is run when the robot is first started up and should be
 	 * used for any initialization code.
 	 */
+
+	File file_joy, file_gyro, file_encoder;
+	FileWriter out_joy, out_gyro, out_encoder;
+	boolean writing = false;
+	
+	Joystick joy = new Joystick(0);
+	WPI_TalonSRX left1 = new WPI_TalonSRX(11), left2= new WPI_TalonSRX(12);
+	WPI_TalonSRX right1 = new WPI_TalonSRX(13), right2 = new WPI_TalonSRX(14);
+	
+	SpeedControllerGroup left = new SpeedControllerGroup(left1, left2);
+	SpeedControllerGroup right = new SpeedControllerGroup(right1, right2);
+	
+	DifferentialDrive drive = new DifferentialDrive(left, right);
+	
+	AHRS gyro = new AHRS(I2C.Port.kMXP);
+	
 	@Override
 	public void robotInit() {
-		File file = new File("/home/lvuser/Log.txt");
+		
+		long time = System.currentTimeMillis();
+		file_joy = new File("/media/sda/joy-"+time+".txt");
+		file_gyro = new File("/media/sda/gyro-"+time+".txt");
+		file_encoder = new File("/media/sda/encoder-"+time+".txt");
 		
 		try {
-			FileWriter out = new FileWriter(file);
-			out.write("Hello Birk, I am on the RoboRIO");
-			
-			out.close();
+			out_joy = new FileWriter(file_joy);
+			out_gyro = new FileWriter(file_gyro);
+			out_encoder = new FileWriter(file_encoder);
+
+			writing = true;
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
 		
+		gyro.enableLogging(true);
 		
-		/*
-		FileOutputStream out;
-		try {
-			out = new FileOutputStream(file);
-			byte[] bytes = {(byte)'H',(byte)'E',(byte)'L',(byte)'L',(byte)'O'};
-			out.write(bytes);
-			
-		} catch (FileNotFoundException e) {
-			e.printStackTrace();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-		*/
-		
-		
-	}
+	}//robotInit
 
 	/**
 	 * This autonomous (along with the chooser code above) shows how to select
@@ -70,6 +84,16 @@ public class Robot extends IterativeRobot {
 	 */
 	@Override
 	public void autonomousInit() {
+		if (writing) {
+			try {
+				out_joy.close();
+				out_gyro.close();
+				out_encoder.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+		
 	}
 
 	/**
@@ -84,7 +108,38 @@ public class Robot extends IterativeRobot {
 	 */
 	@Override
 	public void teleopPeriodic() {
-	}
+		double x = joy.getRawAxis(0);
+		double y = joy.getRawAxis(1);
+		
+		double angle = gyro.getAngle();
+		int encoder = left1.getSelectedSensorPosition(0);
+		
+		drive.arcadeDrive(y, x);
+		
+		if (writing) {
+			try {
+				long time = System.currentTimeMillis();
+				out_joy.write(time+"--"+String.format("%f,%f\n", x, y));
+				out_gyro.write(time+"--"+String.format("%f\n", angle));
+				out_encoder.write(time+"--"+String.format("%d\n", encoder));
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+		
+	}//teleopPeriodic
+	
+	public void disabledInit() {
+		if (writing) {
+			try {
+				out_joy.write("\n");
+				out_gyro.write("\n");
+				out_encoder.write("\n");
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+	}//disabledInit
 
 	/**
 	 * This function is called periodically during test mode.
