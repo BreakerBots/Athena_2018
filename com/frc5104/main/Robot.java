fb01abe377fe1a62667a4c422e948bf41b27af73
Ol' Dell <5104breakerbots@gmail.com>
2018-01-27 17:35:15 -0800

    Added subsystem shell

---
+++ com/frc5104/main/Robot.java
@@ -0,0 +1,27 @@
+package com.frc5104.main;
+
+import edu.wpi.first.wpilibj.IterativeRobot;
+
+public class Robot extends IterativeRobot {
+
+	public void robotInit() {
+		
+	}//robotInit
+	
+	public void autonomousInit() {
+		
+	}//autonomousInit
+	
+	public void autonomousPeriodic() {
+		
+	}//autonomousPeriodic
+	
+	public void teleopInit() {
+		
+	}//teleopInit
+	
+	public void teleopPeriodic() {
+		
+	}//teleopPeriodic
+	
+}//Robot
---
+++ com/frc5104/main/subsystems/Drive.java
@@ -0,0 +1,5 @@
+package com.frc5104.main.subsystems;
+
+public class Drive {
+
+}
