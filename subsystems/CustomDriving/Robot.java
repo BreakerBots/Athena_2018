0b9990d9ef1d876be35839391ea9fc1727dd3aff
Liam Snow <liamsnow03@gmail.com>
2018-02-15 15:19:11 -0800

    Update Robot.java

--- subsystems/CustomDriving/Robot.java
+++ subsystems/CustomDriving/Robot.java
@@ -17,44 +17,11 @@ import edu.wpi.first.wpilibj.vision.VisionPipeline;
 import edu.wpi.first.wpilibj.vision.VisionThread;
 
 public class Robot extends IterativeRobot {
-	private VisionThread visionThread;
-	private double centerX = 0.0;
-	
-	private final Object imgLock = new Object();
-	
-	TalonSRX talon1 = new TalonSRX(11);
-	TalonSRX talon2 = new TalonSRX(12);
-	TalonSRX talon3 = new TalonSRX(13);
-	TalonSRX talon4 = new TalonSRX(14);
-	CustomDrive drive = new CustomDrive(talon1, talon2, talon3, talon4);
+	CustomDrive drive = new CustomDrive(11, 12, 13, 14);
 	
 	Joystick controller = new Joystick(0);
-	
-//	public void robotInit() {
-//	    UsbCamera camera = CameraServer.getInstance().startAutomaticCapture();
-//	    camera.setResolution(640, 480);
-//	    
-//	    visionThread = new VisionThread(camera, new MVisionPipeline(), pipeline -> {
-//	        if (!pipeline.filterContoursOutput().isEmpty()) {
-//	            Rect r = Imgproc.boundingRect(pipeline.filterContoursOutput().get(0));
-//	            synchronized (imgLock) {
-//	                centerX = r.x + (r.width / 2);
-//	            }
-//	        }
-//	    });
-//	    visionThread.start();	        
-//	}
 	
 	public void teleopPeriodic() {
 		drive.ArcadeDrive(controller.getRawAxis(0), controller.getRawAxis(1));
 	}
-	
-//	public void autonomousPeriodic() {
-//	    double centerX;
-//	    synchronized (imgLock) {
-//	        centerX = this.centerX;
-//	    }
-//	    double turn = centerX - (640 / 2);
-//	    drive.ArcadeDrive(-0.6, turn * 0.005);
-//	}
 }
