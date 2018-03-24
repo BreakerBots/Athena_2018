3d8c09a813b1c6c8d72b564f8d7664d9881a214e
Ol' Dell <5104breakerbots@gmail.com>
2018-03-23 20:18:46 -0700

    Moved CSVFileWriter.Data to Column

---
+++ com/frc5104/logging/Column.java
@@ -0,0 +1,31 @@
+package com.frc5104.logging;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Column {
+	public final String name;
+	public final LogDouble callback;
+	
+	public List<Double> values;
+	
+	public Column (String name, LogDouble callback) {
+		this.name = name;
+		this.callback = callback;
+		
+		values = new ArrayList<Double>();
+	}//Data
+	
+	public void collect() {
+		values.add(callback.get());
+	}//collect
+	
+	public int size() {
+		return values.size();
+	}//age
+	
+	public double getValue(int index) {
+		return values.get(index);
+	}//getValue
+	
+}//Column
